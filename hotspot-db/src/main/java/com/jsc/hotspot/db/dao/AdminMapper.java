438df67602b431d7a24dc97f932317b61adbfbd3
tzm <1184971211@qq.com>
2019-11-07 09:38:22 +0800

    修改了mode数据数据类型

--- hotspot-db/src/main/java/com/jsc/hotspot/db/dao/AdminMapper.java
+++ hotspot-db/src/main/java/com/jsc/hotspot/db/dao/AdminMapper.java
@@ -2,10 +2,8 @@ package com.jsc.hotspot.db.dao;
 
 import com.jsc.hotspot.db.domain.Admin;
 import com.jsc.hotspot.db.domain.AdminExample;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
-
+import org.apache.ibatis.annotations.Param;
 
 public interface AdminMapper {
     /**
